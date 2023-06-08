c5711788432e8df279b0f393ed90c5f3fff00572
Allen <zhangzhao2010@yeah.net>
2023-06-08 23:24:52 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # aws-lambda-sqs-to-s3
 receive messages from sqs, save all of messages to a file, upload the file to s3 bucket.
+
+# Lambda 配置
+Python 3.10
+
+# TIPS
+1. Lambda 执行超时时间设置到最长（15分钟）
+2. Lambda 函数执行角色需要附加"AmazonSQSFullAccess"和"AmazonS3FullAccess"两个权限策略
+3. 如果队列中消息数过多，建议自行增加消息数判断逻辑（例如读取超过1000条消息后立马退出循序）
---
+++ lambda_function.py
@@ -0,0 +1,54 @@
+import boto3
+import time
+import json
+
+def lambda_handler(event, context):
+    # 初始化 AWS 客户端
+    sqs = boto3.client('sqs')
+    s3 = boto3.client('s3')
+
+    # 设置 SQS 和 S3 的相关信息
+    queue_url = 'SQS_URL'
+    bucket_name = 'BUCKET_NAME'
+    output_file = 'messages-{}.json'.format(time.time())
+    
+    messages = []
+    finished = False
+
+    while not finished:
+        # 从 SQS 中获取一批消息
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=['All'],
+            MaxNumberOfMessages=10,
+            VisibilityTimeout=0,  # 立即使消息可见
+            WaitTimeSeconds=20  # 设置等待时间，最长为20秒
+        )
+
+        if 'Messages' in response:
+            received_messages = response['Messages']
+
+            for message in received_messages:
+                messages.append(message)
+                # print(json.dumps(message))
+
+                # 删除已读取消息
+                sqs.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=message['ReceiptHandle']
+                )
+        else:
+            finished = True
+
+    # 将消息保存为 JSON 文件
+    with open('/tmp/{}'.format(output_file), 'w') as file:
+        json.dump(messages, file)
+    
+    s3.upload_file('/tmp/{}'.format(output_file), bucket_name, output_file)
+
+    return {
+        'statusCode': 200,
+        'body': 'Messages saved to S3',
+        'bucket_name': bucket_name,
+        'object_name': output_file
+    }
